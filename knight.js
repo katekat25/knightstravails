2588cace24cf423a12009b874ce5ba4ed8f46bc2
Kate Schumacher <kate.schumacher@gmail.com>
2025-03-24 10:45:13 -0500

    Write getPossibleMoves function

--- knight.js
+++ knight.js
@@ -6,6 +6,50 @@ class Node {
     }
 }
 
+function getPossibleMoves(x, y) {
+    function pushValidMove(x, y, xShift, yShift, operation, array) {
+        let boardWidth = 8;
+        let boardHeight = 8;
+        if (x >= boardHeight || y >= boardHeight) {
+            throw new Error("Coordinates must not exceed board height or width.");
+        }
+        let newX = operation(x, xShift);
+        let newY = operation(y, yShift);
+        if (newX >= 0 && newY >= 0 && newX < boardWidth && newY < boardHeight) {
+            if (!array.some(([existingX, existingY]) => existingX === newX && existingY === newY)) {
+                array.push([newX, newY]);
+            }
+        }
+    }
+    let moves = [];
+
+    const operations = {
+        add: (a, b) => a + b,
+        subtract: (a, b) => a - b
+    }
+
+    const shifts = [
+        [1, 2], [2, 1], [-1, 2], [-2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1]
+    ]
+
+    for (let [xShift, yShift] of shifts) {
+        pushValidMove(x, y, xShift, yShift, operations.add, moves);
+        pushValidMove(x, y, xShift, yShift, operations.subtract, moves);
+    }
+
+    return moves;
+}
+
+//ex coord: [4,4]
+[x+1,y+2]
+[x+2,y+1]
+[x+2,y-1]
+[x+1,y-2]
+[x-1,y-2]
+[x-2,y-1]
+[x-2,y+1]
+[x-1,y+2]
+
 function knightMoves(startPoint, endPoint) {
     let queue = [startPoint];
     startPoint.visited = true;
